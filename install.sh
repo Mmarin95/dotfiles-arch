85095a4cb2be43113dfd9bd87e1041bb51d24a19
Mmarin95 <9479635+Mmarin95@users.noreply.github.com>
2023-08-31 21:43:04 +0200

    added nvim basic config

--- install.sh
+++ install.sh
@@ -3,9 +3,11 @@
 # NVIM
 mkdir -p "$HOME/.config/nvim"
 mkdir -p "$HOME/.config/nvim/undo"
-ln -sf "$HOME/dofiles/nvim/init.vim" "$HOME/.config/nvim"
+
+ln -sf "$HOME/dotfiles/nvim/init.vim" "$HOME/.config/nvim"
 
 # Xresources
 rm -rf "$HOME/.config/X11"
 ln -s "$HOME/dotfiles/X11" "$HOME/.config"
+
 
